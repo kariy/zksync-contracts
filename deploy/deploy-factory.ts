c2cce88a823bf42b70e54bf10d7d2c0c44d68363
Kari Evergreen <evergreenkary@gmail.com>
2023-03-18 02:10:11 +0800

    add deployment scripts

--- deploy/deploy-factory.ts
+++ deploy/deploy-factory.ts
@@ -1,4 +1,4 @@
-import { Wallet, utils } from "zksync-web3";
+import { Wallet, Provider, utils } from "zksync-web3";
 import * as ethers from "ethers";
 import { HardhatRuntimeEnvironment } from "hardhat/types";
 import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
@@ -7,3 +7,25 @@ import { Deployer } from "@matterlabs/hardhat-zksync-deploy";
 const PRIVATE_KEY: string =
 	process.env.ZKS_PRIVATE_KEY ||
 	"0x7726827caac94a7f9e1b160f7ea819f172f7b6f9d2a97f992c38edeab82d4110"; // privkey from the local testnet https://github.com/matter-labs/local-setup/blob/main/rich-wallets.json
+
+export default async function (hre: HardhatRuntimeEnvironment) {
+	const provider = Provider.getDefaultProvider();
+	const eoa = new Wallet(PRIVATE_KEY, provider);
+	const deployer = new Deployer(hre, eoa);
+
+	const factoryArtifact = await deployer.loadArtifact("WalletFactory");
+	const accountArtifact = await deployer.loadArtifact("SimpleAccount");
+
+	const factory = await deployer.deploy(
+		factoryArtifact,
+		[utils.hashBytecode(accountArtifact.bytecode)],
+		undefined,
+		[
+			// Since the factory requires the code of the multisig to be available,
+			// we should pass it here as well.
+			accountArtifact.bytecode,
+		]
+	);
+
+	console.log(`AA factory address: ${factory.address}`);
+}
