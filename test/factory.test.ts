f71ae2305a3064e2b946778aa7c39c04ca6c9e5c
Kari <evergreenkary@gmail.com>
2023-03-19 12:09:05 +0800

    Test deploy with paymaster

--- test/factory.test.ts
+++ test/factory.test.ts
@@ -1,5 +1,5 @@
 import { expect } from "chai";
-import { Wallet, Provider, utils } from "zksync-web3";
+import { Wallet, Provider, utils, types, EIP712Signer } from "zksync-web3";
 import * as hre from "hardhat";
 import { Deployer } from "@matterlabs/hardhat-zksync-toolbox";
 import { ethers } from "ethers";
@@ -60,5 +60,68 @@ describe("WalletFactory", function () {
 		);
 
 		expect(await accountContract.isOwner(owner1.address)).to.equal(true);
+	});
+
+	it("Factory should pay for deployment", async function () {
+		const { factory, eoa, provider, accountArtifact } = await setup();
+
+		const salt = ethers.constants.HashZero;
+		const owner1 = Wallet.createRandom();
+		const owner2 = Wallet.createRandom();
+
+		await (
+			await eoa.sendTransaction({
+				to: factory.address,
+				// You can increase the amount of ETH sent to the multisig
+				value: ethers.utils.parseEther("1"),
+			})
+		).wait();
+
+		console.log(
+			"Factory has been funded.",
+			await provider.getBalance(factory.address)
+		);
+
+		const beforeTxBalance = await eoa.getBalance();
+
+		let deployTx = await factory.populateTransaction.deployWallet(salt, [
+			owner1.address,
+			owner2.address,
+		]);
+
+		const paymasterInterface = new ethers.utils.Interface([
+			"function general(bytes data)",
+		]);
+
+		const gasLimit = await provider.estimateGas(deployTx);
+		const gasPrice = await provider.getGasPrice();
+
+		deployTx = {
+			...deployTx,
+			from: eoa.address,
+			gasLimit: gasLimit,
+			gasPrice: gasPrice,
+			chainId: (await provider.getNetwork()).chainId,
+			nonce: await provider.getTransactionCount(eoa.address),
+			type: 113,
+			customData: {
+				gasPerPubdata: utils.DEFAULT_GAS_PER_PUBDATA_LIMIT,
+				paymasterParams: {
+					paymaster: factory.address,
+					paymasterInput: paymasterInterface.encodeFunctionData("general", [
+						[],
+					]),
+				},
+			} as types.Eip712Meta,
+			value: ethers.BigNumber.from(0),
+		};
+
+		const sentTx = await eoa.sendTransaction(deployTx);
+		await sentTx.wait();
+
+		const afterTxBalance = await eoa.getBalance();
+
+		// tx sender balance should be the same before and after sending the tx
+		expect(beforeTxBalance._hex).to.equal(afterTxBalance._hex);
 	});
 });
